62aa65bc2506f87fdba58edc4e48b569f90bbde7
Jishnu Warrier <53039773+jishnuwarrier@users.noreply.github.com>
2022-09-29 09:31:12 +0530

    Time: 534 ms (14.39%), Space: 90.3 MB (32.68%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -14,48 +14,12 @@ public:
             
             sum += num;
             map[sum]++;
-            
-            ans += map[sum - k];
+            int reqd = sum-k;
+            if(map.find(reqd) != map.end())
+                ans += map[reqd];
         }
         
         return ans;
     }
-    
-    
-    //Using Two Pointers
-//     int numberOfSubarrays(vector<int>& nums, int k) {
-            
-//             int n = nums.size();
-//             int ans = 0;
-//             int odd = 0, l = 0, r = 0;
-        
-//             for(int r=0; r<n; r++){
-                
-//                 int len1 = 0, len2 = 0;
-                
-//                 if(nums[r]%2 == 1){
-//                     odd++;
-//                     continue;
-//                 }
-                
-//                 while(odd >= k){
-//                     if(nums[l++]%2 == 1)
-//                         odd--;
-//                 }
-                                
-//                 if(odd == k){
-//                     while(nums[l]%2 == 0)
-//                         len1++, l++;
-                    
-//                     r++;
-//                     while(nums[r] %2 == 0)
-//                         len2++, r++;
-                    
-//                     ans += len1*len2;
-//                 }
-                    
-//             }
-//         cout<<l<<" "<<r;
-//         return ans;
-//     }
+
 };
