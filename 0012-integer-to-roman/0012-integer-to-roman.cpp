56892236430b48525eceb9a820901f19fa6a8a77
Jishnu Warrier <53039773+jishnuwarrier@users.noreply.github.com>
2022-10-20 10:32:48 +0530

    Time: 15 ms (55.72%), Space: 6 MB (47.60%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -3,59 +3,13 @@ public:
     string intToRoman(int num) {
         
         string ans;
+        string rom[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int val[] =    {1000, 900, 500,  400, 100,  90,   50,  40,   10,   9,   5,   4,    1};
         
-        while(num > 0){
-            while(num >= 1000)
-                ans += 'M',
-                num -= 1000;
-            
-            if(num >= 900)
-                ans += "CM",
-                num -= 900;
-            
-            if(num >= 500)
-                ans += 'D',
-                num -= 500;
-            
-            if(num >= 400)
-                ans += "CD",
-                num -= 400;
-            
-            while(num >= 100)
-                ans += 'C',
-                num -= 100;
-            
-            if(num >= 90)
-                ans += "XC",
-                num -= 90;
-            
-            if(num >= 50)
-                ans += 'L',
-                num -= 50;
-            
-            if(num >= 40)
-                ans += "XL",
-                num -= 40;
-            
-            while(num >= 10)
-                ans += 'X',
-                num -= 10;
-            
-            if(num == 9)
-                ans += "IX",
-                num -=9;
-            
-            if(num >= 5)
-                ans += 'V',
-                num -=5;
-            
-            if(num == 4)
-                ans += "IV",
-                num -=4;
-            
-            while(num >= 1)
-                ans += 'I',
-                num--;
+        for(int i=0; num > 0; i++){
+            while(val[i] <= num)
+                ans += rom[i],
+                num -= val[i];
         }
         
         return ans;
