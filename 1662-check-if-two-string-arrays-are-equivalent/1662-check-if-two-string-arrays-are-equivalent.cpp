7c09acbf2c7f5bf6d26fc4f27cf7369e4fbfc3c5
Jishnu Warrier <53039773+jishnuwarrier@users.noreply.github.com>
2022-10-25 17:16:37 +0530

    Time: 21 ms (5.57%), Space: 11.4 MB (77.71%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
@@ -1,16 +1,10 @@
 class Solution {
-public:
-    string createstring(vector<string>&word){
-        string ans;
-        for(auto str: word)
-            ans += str;
-        
-        return ans;
-    }
-    
-    
+public:    
     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
 
-        return (createstring(word1) == createstring(word2));
+        string s1, s2;
+        for(auto str: word1) s1 += str;
+        for(auto str: word2) s2 += str;
+        return s1 == s2;
     }
 };
