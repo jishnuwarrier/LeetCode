37bcb804a0cf6baef28963de2df961456fdc75fb
Jishnu Warrier <53039773+jishnuwarrier@users.noreply.github.com>
2022-10-31 21:12:50 +0530

    Time: 0 ms (100.00%), Space: 7 MB (95.05%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.cpp
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -1,34 +1,18 @@
 class Solution {
-public:
-    
-    int n;
-    vector<string> ans;
-    
-    void dfs(vector<string>& wordDict, string s, string str, int i){
-        if(i == n){
-            ans.push_back(str.substr(0, str.size()-1)); 
-            return;
-        }
+public:  
+    void dfs(vector<string> &ans, vector<string>& wordDict, string s, string sentence, int i){
+        if(i == s.size())
+            ans.push_back(sentence.substr(0, sentence.size()-1)); 
         
-        for(auto word:wordDict){
-            if(i+word.size()<=n && s.substr(i, word.size()) == word){
-                dfs(wordDict, s, str+word+" ", i+word.size());
-            }
-        }        
+        else
+            for(auto word:wordDict)
+                if(i+word.size()<=s.size() && s.substr(i, word.size()) == word)
+                    dfs(ans, wordDict, s, sentence+word+" ", i+word.size());
     }
     
     vector<string> wordBreak(string s, vector<string>& wordDict) {
-        
-        n = s.size();
-        
-        for(auto word:wordDict){
-            if(word.size()==n && s==word)
-                ans.push_back(word);
-    
-            if(word.size()<n && s.substr(0, word.size()) == word){
-                dfs(wordDict, s, word+" ", word.size());
-            }
-        }
+        vector<string> ans;
+        dfs(ans, wordDict, s, "", 0);
         return ans;
     }
 };
