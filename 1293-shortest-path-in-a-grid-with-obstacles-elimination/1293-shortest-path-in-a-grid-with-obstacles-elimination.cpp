3d53abdb4d4794c3e60a49a4659dc721321cfb29
Jishnu Warrier <53039773+jishnuwarrier@users.noreply.github.com>
2022-10-31 11:28:13 +0530

    Time: 284 ms (48.10%), Space: 36.6 MB (53.04%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -1,38 +1,35 @@
 class Solution {
 public:
 
+    struct triplet{
+        int x,y,z;
+    };
+    
     int shortestPath(vector<vector<int>>& grid, int k) {
         
         vector<vector<int>> dir = {{0,1}, {0,-1}, {-1,0}, {1,0}};
         int m=grid.size(), n=grid[0].size();
-        vector<vector<int>> vis(m,vector<int>(n,-1));
-        queue<vector<int>> q;
+        vector<vector<vector<bool>>> vis(m,vector<vector<bool>>(n, vector<bool>(k+1)));
+        queue<triplet> q;
         q.push({0,0,k});
         int step = 0;
         
         for(; !q.empty(); step++){
             int size = q.size();
             while(size-- > 0){
-                int x = q.front()[0];
-                int y=q.front()[1];
-                int newk = q.front()[2];
+                auto [x,y,z] = q.front();
                 q.pop();
-                 
-                if(x==m-1 && y==n-1) return step;
-                if(x<0 || x>=m || y<0 || y>=n) continue;
-                if(vis[x][y]!=-1 && vis[x][y]>=newk)continue;
-            
                 
-                if(grid[x][y]==1){
-                    if(newk > 0) newk--;
-                    else continue;
-                }
-                vis[x][y] = newk;
+                if(x<0||x==m||y<0||y==n||z<0||vis[x][y][z]) continue;
+                if(x==m-1 && y==n-1) return step;
+                
+                vis[x][y][z]=true;
                 
                 for(int i=0; i<4; i++){
-                    int x1 = x+dir[i][0], y1 = y+dir[i][1];
-                    q.push({x1,y1,newk});
-                }                 
+                    int x1=x+dir[i][0], y1=y+dir[i][1];
+                    q.push({x1, y1, z-grid[x][y]});
+                }
+                
             }
         }
         return (-1);
