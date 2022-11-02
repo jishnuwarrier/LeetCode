94ffcf917bce48b63f6964dcfa92ecb17b443bbd
Jishnu Warrier <53039773+jishnuwarrier@users.noreply.github.com>
2022-11-02 12:13:35 +0530

    Time: 15 ms (41.10%), Space: 7.1 MB (50.06%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -2,33 +2,41 @@ class Solution {
 public:
     int m, n;
     string s,p;
+    vector<vector<int>> dp;
     
-    bool dfs(int i, int j){
+    int dfs(int i, int j){
 
-        if(i>=m && j>=n) return true;
-        if(j>=n) return false;
+        if(i>=m && j>=n) return 1;
+        if(j>=n) return -1;
         
         if(i>=m){
             while(j<n-1 && p[j+1]=='*')
                 j+=2;
             
-            return (j>=n);
+            return (j<n)? -1 : 1;
         }
+        
+        if(dp[i][j] != 0) return dp[i][j];
         
         bool check = (s[i]==p[j] || p[j]=='.');
           
-        if(j<n-1 && p[j+1]=='*')
-            return (check) ? (dfs(i+1,j) || dfs(i,j+2)) : dfs(i,j+2);
+        if(j<n-1 && p[j+1]=='*'){
+            if(check)
+                return dp[i][j] = (max(dfs(i+1,j), dfs(i,j+2)));
+            else
+                return dp[i][j] = dfs(i,j+2);
+        }
         
         else if(check  == true)
-            return dfs(i+1, j+1);
+            return dp[i][j] = dfs(i+1, j+1);
         
-        return false;
+        return -1;
     }
     
     bool isMatch(string s1, string s2) {
         s=s1, p=s2;
         m=s.size(), n=p.size();
-        return dfs(0, 0);
+        dp = vector<vector<int>>(m, vector<int>(n, 0));
+        return (dfs(0, 0) == -1) ? false : true;
     }
 };
