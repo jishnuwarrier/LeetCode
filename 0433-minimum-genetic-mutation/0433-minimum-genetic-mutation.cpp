3a7df4078e97628d8f0b43922288a62a0eab4972
Jishnu Warrier <53039773+jishnuwarrier@users.noreply.github.com>
2022-11-02 21:28:17 +0530

    Time: 2 ms (49.64%), Space: 6.5 MB (96.88%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
@@ -1,7 +1,5 @@
 class Solution {
 public:
-    int ans = INT_MAX;
-    unordered_map<string, vector<string>>map;
     
     bool dist(string s1, string s2){
         int count = 0;
@@ -11,37 +9,28 @@ public:
         
         return (count == 1) ? true : false;
     }
-    
-    void dfs(string &gene, string &end, int steps){
-        if(steps > 10)
-            return;
         
-        if(gene == end)
-            ans = min(ans, steps);
-        
-        for(auto str: map[gene]){
-            dfs(str, end, steps+1);
-        }
-    }
-    
     int minMutation(string start, string end, vector<string>& bank) {
         
-        for(auto str: bank)
-            if(dist(start, str))
-                map[start].push_back(str);
+        unordered_set<string> set;        
+        queue<string> q;
+        q.push(start);
         
-        for(int i=0; i<bank.size(); i++){
-            for(int j=i+1; j<bank.size(); j++){
+        for(int steps=0; !q.empty(); steps++){
+            int size = q.size();
+            while(size-- > 0){
+                string str = q.front();
+                q.pop();
                 
-                if(i==j) continue;
+                if(str == end) return steps;
                 
-                if(dist(bank[i], bank[j]))
-                    map[bank[i]].push_back(bank[j]),
-                    map[bank[j]].push_back(bank[i]);
-            }   
+                for(int i=0; i<bank.size(); i++){
+                    if(dist(str, bank[i]) && set.find(bank[i])==set.end())
+                        set.insert(bank[i]), q.push(bank[i]);
+                }
+            }
         }
         
-        dfs(start, end, 0);
-        return (ans == INT_MAX) ? -1 : ans;
+        return -1;
     }
 };
