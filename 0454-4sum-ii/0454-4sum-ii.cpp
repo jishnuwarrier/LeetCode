5688bfb2aa16124b8c313dbfe8d10ba954debcb0
Jishnu Warrier <53039773+jishnuwarrier@users.noreply.github.com>
2022-11-11 23:52:01 +0530

    Time: 917 ms (5.01%), Space: 24.8 MB (31.31%) - LeetHub

---
+++ 0454-4sum-ii/0454-4sum-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+
+        int ans = 0;
+        int n = nums1.size();
+        
+        map<int, int> map;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                map[nums1[i]+nums2[j]]++;
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(map.find(-nums3[i]-nums4[j]) != map.end())
+                    ans += map[-nums3[i]-nums4[j]];
+            }
+        }
+        
+        return ans;
+    }
+};
