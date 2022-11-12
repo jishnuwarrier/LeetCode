0742ce0d950bf2a304d62f9e3762c8f5f63b67e8
Jishnu Warrier <53039773+jishnuwarrier@users.noreply.github.com>
2022-11-13 04:06:27 +0530

    Time: 479 ms (78.10%), Space: 117 MB (65.01%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -9,42 +9,14 @@ public:
     
     void addNum(int num) {
                 
-        if(left.size() == 0)
-            left.push(num);
+        left.push(num);
+        right.push(left.top());
+        left.pop();
         
-        else if(right.size() == 0){
-            if(num >= left.top())
-                right.push(num);
-            
-            else{
-                right.push(left.top()); 
-                left.pop();
-                left.push(num);
-            }
+        if(left.size() < right.size()){
+            left.push(right.top());
+            right.pop();
         }
-        
-        else if(left.size() <= right.size()){
-            if(num <= right.top())
-                left.push(num);
-            
-            else{
-                left.push(right.top());
-                right.pop();
-                right.push(num);
-            }
-        }
-        
-        else{
-            if(num >= left.top())
-                right.push(num);
-            
-            else{
-                right.push(left.top());
-                left.pop();
-                left.push(num);
-            }
-        }
-            
     }
     
     double findMedian() {
@@ -52,7 +24,7 @@ public:
             return ((double)left.top() + right.top())/2;
         
         
-        return (left.size() > right.size()) ? left.top() : right.top();
+        return left.top();
     }
 };
 
