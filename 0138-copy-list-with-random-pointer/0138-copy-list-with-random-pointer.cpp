350622bd4acc9809397d9152cb222483b2d21db4
Jishnu Warrier <53039773+jishnuwarrier@users.noreply.github.com>
2022-11-24 03:43:14 +0530

    Time: 24 ms (11.07%), Space: 11.4 MB (17.40%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -16,43 +16,21 @@ public:
 
 class Solution {
 public:
-    
-    int n = 0;
-    
-    int nodeno(Node* head){
-        int posfromend = 0;
-        for(Node* curr=head; curr; curr=curr->next, posfromend++);
-        return n-posfromend;
-    }
-    
+
     Node* copyRandomList(Node* head) {
         
         if(!head) return NULL;
-        for(Node* curr = head; curr; curr=curr->next, n++);
-        vector<int> rand(n);
+        map<Node*, Node*> map;
+        map[head] = new Node(head->val);
         
-        int i=0;
-        for(Node* curr = head; curr; curr=curr->next, i++)
-            rand[i] = nodeno(curr->random);
-
-        Node* newhead = new Node(head->val);
-        Node* curr = head;
-        Node* newcurr = newhead;
-        
-        map<int, Node*> map;
-        map[n] = NULL;
-        for(int i=0; i<n-1; i++, curr=curr->next, newcurr=newcurr->next){
-            newcurr->val = curr->val;
-            newcurr->next = new Node(curr->next->val);
-            map[i] = newcurr;
+        for(Node* curr=head; curr->next; curr=curr->next){
+            map[curr]->next = new Node(curr->next->val);
+            map[curr->next] = map[curr]->next;
         }
-        map[n-1] = newcurr;
-        newcurr->next = NULL;
-        
-        newcurr = newhead;
-        for(int i=0; i<n; i++, newcurr=newcurr->next)
-            newcurr->random = map[rand[i]];
 
-        return newhead;
+        for(Node* curr=head; curr; curr=curr->next)
+            map[curr]->random = map[curr->random];
+        
+        return map[head];
     }
 };
