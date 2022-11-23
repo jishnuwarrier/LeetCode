9f62f3feb3cfcb34407d2c2788d7fa484015a458
Jishnu Warrier <53039773+jishnuwarrier@users.noreply.github.com>
2022-11-24 00:58:11 +0530

    Time: 61 ms (21.94%), Space: 19.4 MB (47.87%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,51 +1,21 @@
 class Solution {
 public:
-    
-    bool checkrow(vector<vector<char>>& board, int row){
-        
-        set<int> s;
-        for(int j=0; j<9; j++){
-            if(board[row][j] == '.') continue;
-            if(s.find(board[row][j]) == s.end()) s.insert(board[row][j]);
-            else return false;
-        }
-        return true;
-    }
-    
-    bool checkcol(vector<vector<char>>& board, int col){
-        
-        set<int> s;
-        for(int i=0; i<9; i++){
-            if(board[i][col] == '.') continue;
-            if(s.find(board[i][col]) == s.end()) s.insert(board[i][col]);
-            else return false;
-        }
-        return true;
-    }
-    
-    bool checkbox(vector<vector<char>>& board, int box){
-        
-        set<int> s;
-        int x = 3*(box/3), y = 3*(box%3);
-        
-        for(int a=0; a<9; a++){
-            int i = x+a/3, j=y+a%3;
-            if(board[i][j] == '.') continue;
-            if(s.find(board[i][j]) == s.end()) s.insert(board[i][j]); 
-            else return false;    
-        }
-        return true;
-    }
-    
+
     bool isValidSudoku(vector<vector<char>>& board) {
         
-        bool ans = true;
-        
+        vector<vector<char>> row(9, vector<char>(9, false));
+        vector<vector<char>> col(9, vector<char>(9, false));
+        vector<vector<char>> box(9, vector<char>(9, false));
+
         for(int i=0; i<9; i++){
-            if(ans == false) return false;
-            ans = ans & checkrow(board, i) & checkcol(board, i) & checkbox(board, i);
+            for(int j=0; j<9; j++){
+                if(board[i][j] == '.') continue;
+                int val = board[i][j] - '1';
+                if(row[i][val] || col[j][val] || box[3*(i/3)+j/3][val]) return false;
+                row[i][val] = col[j][val] = box[3*(i/3)+j/3][val] = true;
+            }
         }
         
-        return ans;
+        return true;
     }
 };
