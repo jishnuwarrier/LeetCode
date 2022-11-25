87a5f86fcc0b3dfbdb244f6b00a84afcd69cc56c
Jishnu Warrier <53039773+jishnuwarrier@users.noreply.github.com>
2022-11-25 16:41:47 +0530

    Time: 16 ms (39.82%), Space: 13.3 MB (29.42%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -33,15 +33,6 @@ public:
                 }
             }
         }
-        
-        
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(grid[i][j] == 1)
-                    return -1;
-            }
-        }
-        
-        return steps;
+        return (fresh > 0) ? -1 : steps;
     }
 };
