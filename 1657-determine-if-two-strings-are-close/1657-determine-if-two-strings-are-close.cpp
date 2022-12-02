399effa18348070406f971d7315f3a49280d3d36
Jishnu Warrier <53039773+jishnuwarrier@users.noreply.github.com>
2022-12-02 09:28:54 +0530

    Time: 103 ms (88.92%), Space: 20.6 MB (50.30%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -4,22 +4,20 @@ public:
         
         if(word1.size() != word2.size()) return false;
         
-        int n = word1.size();
-        
-        unordered_set<char> s1, s2;
-        vector<int> freq1(26, 0);
-        vector<int> freq2(26, 0);
+        int n = word1.size();        
+        vector<int> freq1(26, 0), freq2(26, 0), c1(26, 0), c2(26, 0);
         
         for(int i=0; i<n; i++){
             freq1[word1[i]-'a']++;
             freq2[word2[i]-'a']++;
             
-            s1.insert(word1[i]); s2.insert(word2[i]);
+            c1[word1[i] - 'a'] = 1;
+            c2[word2[i] - 'a'] = 1;
         }
         
         sort(freq1.begin(), freq1.end());
         sort(freq2.begin(), freq2.end());
 
-        return (freq1==freq2 && s1==s2) ? true : false;
+        return (freq1==freq2 && c1==c2) ? true : false;
     }
 };
