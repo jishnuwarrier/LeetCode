7f8f3d758d3fb1d9b7fb447380dfa86a983e58ac
Jishnu Warrier <53039773+jishnuwarrier@users.noreply.github.com>
2022-12-04 11:02:01 +0530

    Time: 29 ms (45.70%), Space: 8.5 MB (67.27%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,27 +1,23 @@
-struct comp {
-    template <typename T>
-  
-    bool operator()(const T& l, const T& r) const {
-        return (l.second != r.second) ? l.second > r.second : l.first < r.first;
-    }
-};
-
 class Solution {
 public:
-    
     string frequencySort(string s) {
         unordered_map<char, int> map;
-        vector<pair<char, int>> v;
+        priority_queue<pair<int,char>> pq;
         
         for(auto ch: s)
             map[ch]++;
         
-        set<pair<char, int>, comp> set(map.begin(), map.end());
+        
+        for(auto it: map)
+            pq.push({it.second, it.first});
+        
         string ans;
         
-        for(auto it: set){
-            for(int i=0; i<it.second; i++){
-                ans += it.first;
+        while(pq.size()>0){
+            auto it = pq.top();
+            pq.pop();
+            for(int i=0; i<it.first; i++){
+                ans += it.second;
             }
         }
         
