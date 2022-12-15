1a6e0afbbf8d8cd9922db1b51f8fc4414c0de217
Jishnu Warrier <53039773+jishnuwarrier@users.noreply.github.com>
2022-12-16 01:52:56 +0530

    Time: 2225 ms (6.93%), Space: 529.1 MB (5.64%) - LeetHub

--- 0939-minimum-area-rectangle/0939-minimum-area-rectangle.cpp
+++ 0939-minimum-area-rectangle/0939-minimum-area-rectangle.cpp
@@ -3,7 +3,7 @@ public:
     int minAreaRect(vector<vector<int>>& points) {
         
         int ans = INT_MAX;
-        unordered_map<int, set<int>> xmap;
+        unordered_map<int, unordered_set<int>> xmap;
 
         for(auto point: points){
             xmap[point[0]].insert(point[1]);
@@ -13,13 +13,17 @@ public:
             if(it1.second.size()>=2){
                 for(auto it2: xmap){
                     if(it2.second.size()>=2 && it1!=it2){
+                        unordered_set<int> s1 = it1.second;
+                        unordered_set<int> s2 = it2.second;
+
                         vector<int> common;
 
-                        for(auto y: it1.second){
-                            if(it2.second.find(y) != it2.second.end())
+                        for(auto y: s1){
+                            if(s2.find(y) != s2.end())
                                 common.push_back(y);
                         }
 
+                        sort(common.begin(), common.end());
                         int l = abs(it1.first - it2.first);
 
                         for(int i=1; i<common.size(); i++){
